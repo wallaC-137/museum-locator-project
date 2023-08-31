8e42192152c48bda852920140f18b254e017c214
wallaC-137 <wallace_031@hotmail.com>
2023-08-31 17:33:32 -0300

    começando projeto

--- src/main/java/com/betrybe/museumfinder/MuseumFinderApplication.java
+++ src/main/java/com/betrybe/museumfinder/MuseumFinderApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 public class MuseumFinderApplication {
+  //start project
 
   public static void main(String[] args) {
     SpringApplication.run(MuseumFinderApplication.class, args);
